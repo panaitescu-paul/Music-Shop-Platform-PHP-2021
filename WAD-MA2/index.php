71ae9b004575b244f04c2e0c510b9fe1ffd338b0
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-11-25 23:20:59 +0100

    Remade the Login functionality based on Session Cookes

--- WAD-MA2/index.php
+++ WAD-MA2/index.php
@@ -1,6 +1,40 @@
 <?php
 
+    require_once('src/functions.php');
 
-echo("Index page");
+    session_start();
+    debug($_SESSION);
 
+    if (!isset($_SESSION['userID'])) {
+        header('Location: auth/login.php');
+    }
+    $userID = $_SESSION['userID'];
+    $firstName = $_SESSION['firstName'];
+    $lastName = $_SESSION['lastName'];
+    $email = $_SESSION['email'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Music Marketplace</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.5.1.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/dom.js"></script>
+    <script src="js/functions.js"></script>
+</head>
+<body>
+<h1>Music Marketplace</h1>
+
+<p>Index page!!! </p>
+
+<?php
+    include_once('footer.php');
+?>
+</body>
+</html>
--- WAD-MA2/src/api.php
+++ WAD-MA2/src/api.php
@@ -1,44 +1,109 @@
 <?php
     require_once('functions.php');
-
     debug($_POST);
 
-    if (!isset($_POST['entity']) || !isset($_POST['action'])) {
-        leave();
+    session_start();
+    if (!isset($_SESSION['userID'])) {
+        debug('Session variable userID not set.<br>');
+        leave('User not authenticated.');
     } else {
-        $entity = $_POST['entity'];
-        $action = $_POST['action'];
+        if (!isset($_POST['entity']) || !isset($_POST['action'])) {
+            leave();
+        } else {
+            $entity = $_POST['entity'];
+            $action = $_POST['action'];
 
-        switch ($entity) {
-            case 'user':
-                require_once('user.php');
-                $user = new User;
+            switch ($entity) {
+                case 'person':
+                    require_once('person.php');
+                    $person = new Person;
 
-                switch ($action) {
-                    case 'search':
-                        if (!isset($_POST['info'])) {
-                            leave();
-                        } else {
-                            echo json_encode($user->search($_POST['info']));
-                        }
-                        break;
-                    case 'add':
-                        if (!isset($_POST['info'])) {
-                            leave();
-                        } else {
-                            echo json_encode($user->add($_POST['info']));
-                        }
-                        break;
-                    // case 'delete':
-                    //     if (!isset($_POST['personID'])) {
-                    //         leave();
-                    //     } else {
-                    //         echo json_encode($person->delete($_POST['personID']));
-                    //     }
-                    //     break;
-                }
+                    switch ($action) {
+                        case 'search':
+                            if (!isset($_POST['searchText'])) {
+                                leave();
+                            } else {
+                                echo json_encode($person->search($_POST['searchText']));
+                            }
+                            break;
+                        case 'add':
+                            if (!isset($_POST['personName'])) {
+                                leave();
+                            } else {
+                                echo json_encode($person->add($_POST['personName']));
+                            }
+                            break;
+                        case 'delete':
+                            if (!isset($_POST['personID'])) {
+                                leave();
+                            } else {
+                                echo json_encode($person->delete($_POST['personID']));
+                            }
+                            break;
+                    }
 
-                break;
+                    break;
+                case 'user':
+                    require_once('user.php');
+                    $user = new User;
+
+                    switch ($action) {
+                        case 'update':
+                            if (!isset($_POST['email'])) {
+                                leave();
+                            } else {
+                                echo json_encode($user->update($_POST['email'], $_POST['firstName'], $_POST['lastName'], $_POST['password'], $_POST['newPassword']));
+                            }
+                            break;
+                    }
+                    break;
+                case 'session':
+                    switch ($action) {
+                        case 'destroy':
+                            session_destroy();
+                            leave('Session destroyed');
+                            break;
+                    }
+            }
         }
     }
 ?>
+
+
+//
+// <?php
+//     require_once('functions.php');
+//
+//     debug($_POST);
+//
+//     if (!isset($_POST['entity']) || !isset($_POST['action'])) {
+//         leave();
+//     } else {
+//         $entity = $_POST['entity'];
+//         $action = $_POST['action'];
+//
+//         switch ($entity) {
+//             case 'user':
+//                 require_once('user.php');
+//                 $user = new User;
+//
+//                 switch ($action) {
+//                     case 'search':
+//                         if (!isset($_POST['info'])) {
+//                             leave();
+//                         } else {
+//                             echo json_encode($user->search($_POST['info']));
+//                         }
+//                         break;
+//                     case 'add':
+//                         if (!isset($_POST['info'])) {
+//                             leave();
+//                         } else {
+//                             echo json_encode($user->add($_POST['info']));
+//                         }
+//                         break;
+//                 }
+//                 break;
+//         }
+//     }
+// ?>
