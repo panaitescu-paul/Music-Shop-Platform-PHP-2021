75cee0c5e9a542ca62e1a19d2955d44060fc192d
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-11-25 23:18:36 +0100

    Improved the Debug function for logging errors

--- WAD-MA2/src/functions.php
+++ WAD-MA2/src/functions.php
@@ -1,25 +1,33 @@
 <?php
+    define('LOG_FILE_NAME', 'log.htm');
 
-    function leave() {
-        header('Location: ../index.html');
+    function leave($message = 'There was an error.') {
+        echo json_encode($message);
     }
 
     // It debugs the received information to an HTML file
     function debug($info) {
-        define('LOG_FILE_NAME', 'log.htm');
+
+        $fileName = LOG_FILE_NAME;
+        $path = getcwd();
+
+        // If the invoking php file is in the src directory, the log file is set in the root
+        if (substr($path, strlen($path) - 4, 4) === '\src') {
+            $fileName = '../' . $fileName;
+        }
 
         $text = '';
-        if (!file_exists(LOG_FILE_NAME)) {
+        if (!file_exists($fileName)) {
             $text .= '<pre>';
         }
         $text .= '--- ' . date('Y-m-d h:i:s A', time()) . ' ---<br>';
 
-        $logFile = fopen(LOG_FILE_NAME, 'a');
+        $logFile = fopen($fileName, 'a');
 
         if (gettype($info) === 'array') {
             $text .= print_r($info, true);
         } else {
-            $text .= $info;
+            $text .= $info . '<br>';
         }
         fwrite($logFile, $text);
 
