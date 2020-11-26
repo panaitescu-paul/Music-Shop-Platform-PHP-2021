5ea3f00419013c289f40f6116cbd7100b512c18a
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-11-26 22:00:45 +0100

    Added Shopping Cart Page - HTML and CSS

---
+++ user/shopping-cart.php
@@ -0,0 +1,84 @@
+<?php
+    include_once('../fragments/header-user.php');
+?>
+
+<?php
+    require_once('../src/functions.php');
+
+    session_start();
+    debug($_SESSION);
+
+    if (!isset($_SESSION['userID'])) {
+        header('Location: ../auth/login.php');
+    }
+    $userID = $_SESSION['userID'];
+    $firstName = $_SESSION['firstName'];
+    $lastName = $_SESSION['lastName'];
+    $email = $_SESSION['email'];
+?>
+
+<!-- Start of Page content -->
+<div class="container">
+    <h1 class="main-title">Shopping Cart</h1>
+    <div class="resultArea">
+        <table class="table tableList">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Artist Name</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <td>Track 1: Eminem - Rap God [Official]</td>
+                    <td>
+                        <button type="button" class="btn btn-danger btnDelete">Remove</button>
+                    </td>
+                </tr>
+                <tr>
+                    <th>2</th>
+                    <td>Track 2: 50 Cent - In Da Club [Official]</td>
+                    <td>
+                        <button type="button" class="btn btn-danger btnDelete">Remove</button>
+                    </td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td>Track 1: Eminem - Rap God [Official]</td>
+                    <td>
+                        <button type="button" class="btn btn-danger btnDelete">Remove</button>
+                    </td>
+                </tr>
+                <tr>
+                    <th>2</th>
+                    <td>Track 2: 50 Cent - In Da Club [Official]</td>
+                    <td>
+                        <button type="button" class="btn btn-danger btnDelete">Remove</button>
+                    </td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td>Track 1: Eminem - Rap God [Official]</td>
+                    <td>
+                        <button type="button" class="btn btn-danger btnDelete">Remove</button>
+                    </td>
+                </tr>
+                <tr>
+                    <th>2</th>
+                    <td>Track 2: 50 Cent - In Da Club [Official]</td>
+                    <td>
+                        <button type="button" class="btn btn-danger btnDelete">Remove</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<!-- End of Page content -->
+
+<?php
+    include_once('../fragments/footer.php');
+?>
