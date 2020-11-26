a3beaa7c006e765901c6091841353d01238bd456
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-11-26 22:24:03 +0100

    Added Library Artists page with HTML and CSS

---
+++ user/library-artists.php
@@ -0,0 +1,76 @@
+<?php
+    include_once('../fragments/header-user.php');
+?>
+
+<?php
+    require_once('../src/functions.php');
+
+    session_start();
+    debug($_SESSION);
+
+    if (!isset($_SESSION['userID'])) {
+        header('Location: ../auth/login.php');
+    }
+    $userID = $_SESSION['userID'];
+    $firstName = $_SESSION['firstName'];
+    $lastName = $_SESSION['lastName'];
+    $email = $_SESSION['email'];
+?>
+
+<!-- Start of Page content -->
+<div class="container">
+    <h1 class="main-title">Library Artists</h1>
+    <div class="resultArea">
+        <div class="buttons">
+            <button type="button" class="btn btn-primary mb-2 btnLibrary  active" 
+                onclick="window.location.href='/php/WAD-MA2/user/library-artists.php'">Artists
+            </button>
+            <button type="button" class="btn btn-primary mb-2 btnLibrary" 
+                onclick="window.location.href='/php/WAD-MA2/user/library-albums.php'">Albums
+            </button>
+            <button type="button" class="btn btn-primary mb-2 btnLibrary" 
+                onclick="window.location.href='/php/WAD-MA2/user/library-tracks.php'">Tracks
+            </button>
+        </div>
+        <table class="table tableList">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Artist Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <td>Paul Panaitescu</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td>John Smith</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td>Paul Panaitescu</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td>John Smith</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td>Paul Panaitescu</td>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td>John Smith</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<!-- End of Page content -->
+
+<?php
+    include_once('../fragments/footer.php');
+?>
