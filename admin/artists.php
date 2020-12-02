792ea1c99ab60a35e46eb724569ea25c2dc2740f
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-02 04:03:29 +0100

    Added Artist Information Modal

--- admin/artists.php
+++ admin/artists.php
@@ -23,13 +23,42 @@
     <div class="resultArea">
     
         <button type="button" class="btn btn-success mb-2" id="btnAdd">Add Artist</button>
-        <button type="button" class="btn btn-success mb-2" id="btnShowArtists">Show All Artist</button>
+        <button type="button" class="btn btn-success mb-2" id="btnShowArtists">Show All Artists</button>
         <label for="txtName" id="txtNameLabel">Artist Name</label>
         <input type="text" id="searchArtistName" name="Name" required>
         <button type="button" class="btn btn-success mb-2" id="btnSearchArtist">Search Artist</button>
         
         <section id="artistResults">
         </section>
+
+        <!-- Modal-->
+        <div class="modals">
+            <!-- <div id="movieInfo"> -->
+                <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="modalTitle">Information</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+
+                            <!-- Information will be added here-->
+                            <div class="modal-body">
+                                <div id="modalInfoContent1">
+                                </div>
+                                <div id="modalInfoContent2">
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <!-- </div> -->
+        </div>
 
         <!-- <table class="table tableList">
             <thead>
