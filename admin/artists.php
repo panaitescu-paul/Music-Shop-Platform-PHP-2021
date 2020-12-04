fca412898c9d0e813a7b66fc9167abe2ac31ede0
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-04 22:09:19 +0100

    Added page id

--- admin/artists.php
+++ admin/artists.php
@@ -18,7 +18,7 @@
 ?>
 
 <!-- Start of Page content -->
-<div class="container">
+<div class="container" id="page-artists">
 
     <!-- Sidebar navigation -->
     <div class="sidebar">
@@ -28,113 +28,42 @@
 
     <h1 class="main-title">Artists</h1>
     <div class="resultArea">
-    
         <button type="button" class="btn btn-success mb-2 createArtistModal" 
                 id="btnAdd" data-toggle='modal' data-target='#modal'>Create Artist</button>
-        <button type="button" class="btn btn-success mb-2" id="btnShowArtists">Show All Artists</button>
+        <!-- <button type="button" class="btn btn-success mb-2" id="btnShowArtists">Show All Artists</button> -->
         <label for="txtName" id="txtNameLabel">Artist Name</label>
         <input type="text" id="searchArtistName" name="Name" required>
         <button type="button" class="btn btn-success mb-2" id="btnSearchArtist">Search Artist</button>
-        
-    
         <section id="artistResults">
         </section>
 
         <!-- Modal-->
         <div class="modals">
-            <!-- <div id="movieInfo"> -->
-                <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="modalTitle">Information</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
+            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalTitle">Information</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
 
-                            <!-- Information will be added here-->
-                            <div class="modal-body">
-                                <div id="modalInfoContent1">
-                                </div>
-                                <div id="modalInfoContent2">
-                                </div>
+                        <!-- Information will be added here-->
+                        <div class="modal-body">
+                            <div id="modalInfoContent1">
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <!-- TODO: Delete modalInfoContent2 -->
+                            <div id="modalInfoContent2">
                             </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                         </div>
                     </div>
                 </div>
-            <!-- </div> -->
+            </div>
         </div>
-
-        <!-- <table class="table tableList">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Artist Name</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th>1</th>
-                    <td>Paul Panaitescu</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">
-                            <img src="../img/pencil-square.svg" class="icon-delete">
-                        </button>
-                        <button type="button" class="btn btn-warning btnUpdate">
-                            <img src="../img/trash.svg" class="icon-update">
-                        </button>
-                        <button type="button" class="btn btn-success btnShow">
-                            <img src="../img/card-text.svg" class="icon-show" >
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td>John Smith</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td>Paul Panaitescu</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td>John Smith</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td>Paul Panaitescu</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td>John Smith</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table> -->
     </div>
 </div>
 
