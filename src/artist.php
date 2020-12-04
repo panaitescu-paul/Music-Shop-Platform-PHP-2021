d82d9d054e45dd0ffd40b2ce411f38f65fbde5c3
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-04 17:22:25 +0100

    Added Create Artist in DB functionality

--- src/artist.php
+++ src/artist.php
@@ -35,60 +35,24 @@
         //     return $results;                
         // }
 
-        // /**
-        //  * Inserts a new movie
-        //  * 
-        //  * @param   movie info
-        //  * @return  the ID of the new movie
-        //  */
-        // function create($info) {            
+        /**
+         * Inserts a new Artist
+         * 
+         * @param   artist info
+         * @return  the ID of the new artist
+         */
+        function create($info) {            
+            $query = <<<'SQL'
+                INSERT INTO artist (Name) VALUES (?);
+                SQL;
 
-        //     try {
-        //         $this->pdo->beginTransaction();
-
-        //         $query = <<<'SQL'
-        //             INSERT INTO artist (title, overview, release_date, runtime) VALUES (?, ?, ?, ?);
-        //         SQL;
-
-        //         $stmt = $this->pdo->prepare($query);
-        //         $stmt->execute([$info['title'], $info['overview'], $info['releaseDate'], $info['runtime']]);
-
-        //         $newID = $this->pdo->lastInsertId();
-
-        //         // Directors
-        //         if (isset($info['directors'])) {
-        //             foreach($info['directors'] as $director) {
-        //                 $query = <<<'SQL'
-        //                     INSERT INTO movie_director (movie_id, person_id) VALUES (?, ?);
-        //                 SQL;
-        //                 $stmt = $this->pdo->prepare($query);
-        //                 $stmt->execute([$newID, $director]);
-        //             }
-        //         }
-
-        //         // Actors
-        //         if (isset($info['actors'])) {
-        //             foreach($info['actors'] as $actor) {
-        //                 $query = <<<'SQL'
-        //                     INSERT INTO movie_cast (movie_id, person_id) VALUES (?, ?);
-        //                 SQL;
-        //                 $stmt = $this->pdo->prepare($query);
-        //                 $stmt->execute([$newID, $actor]);
-        //             }
-        //         }
-
-        //         $this->pdo->commit();
-                
-        //     } catch (Exception $e) {
-        //         $newID = -1;
-        //         $this->pdo->rollBack();
-        //         debug($e);
-        //     }
-
-        //     $this->disconnect();
-
-        //     return $newID;
-        // }
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute([$info['name']]);
+            $newID = $this->pdo->lastInsertId();
+            
+            $this->disconnect();
+            return $newID;
+        }
 
         /**
          * Retrieves the artists whose name includes a certain text
