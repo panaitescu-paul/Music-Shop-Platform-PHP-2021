0d6e7ee0becec08c543592c92b61f6bdb125dba6
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-08 11:26:42 +0100

    Track class: getAll

---
+++ src/track.php
@@ -0,0 +1,35 @@
+<?php
+// TODO: Add API documentation
+
+/**
+ * Track class
+ *
+ * @author Paul Panaitescu
+ * @version 1.0 5 DEC 2020:
+ */
+    require_once('connection.php');
+    require_once('functions.php');
+
+    // TODO: Add Try and Catch blocks for every query
+    class Track extends DB {
+
+        /**
+         * Retrieves all tracks 
+         * 
+         * @return  an array with all tracks and their information
+         */
+        
+        function getAll() {
+            $query = <<<'SQL'
+                SELECT TrackId, Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice
+                FROM track;
+            SQL;
+
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
+            $this->disconnect();
+            return $stmt->fetchAll(); 
+        }
+
+    }
+?>
