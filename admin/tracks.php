d70ad6850e8046efff989fdd5619601ca436699a
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-08 11:12:43 +0100

    tracks page HTML

--- admin/tracks.php
+++ admin/tracks.php
@@ -17,70 +17,55 @@
     // $email = $_SESSION['email'];
 ?>
 
+
 <!-- Start of Page content -->
-<div class="container">
+<div class="container" id="page-artists">
+
+    <!-- Sidebar navigation -->
+    <div class="sidebar">
+        <button type="button" class="btn btn-primary scrollUp">Up</button>
+        <button type="button" class="btn btn-primary scrollDown">Down</button>
+    </div>
+
     <h1 class="main-title">Tracks</h1>
     <div class="resultArea">
-        <button type="button" class="btn btn-success mb-2" id="btnAdd">Add Track</button>
-        <table class="table tableList">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Artist Name</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th>1</th>
-                    <td>Paul Panaitescu</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td>John Smith</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td>Paul Panaitescu</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td>John Smith</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td>Paul Panaitescu</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th>1</th>
-                    <td>John Smith</td>
-                    <td>
-                        <button type="button" class="btn btn-danger btnDelete">Delete</button>
-                        <button type="button" class="btn btn-warning btnUpdate">Update</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <button type="button" class="btn btn-success mb-2 createTrackModal" 
+                id="btnAdd" data-toggle='modal' data-target='#modal'>Create Tracks</button>
+        <!-- <button type="button" class="btn btn-success mb-2" id="btnShowArtists">Show All Artists</button> -->
+        <label for="txtName" id="txtNameLabel">Track Name</label>
+        <input type="text" id="searchTrackName" name="Name" required>
+        </br>
+        <button type="button" class="btn btn-success mb-2" id="btnSearchTrack">Search Track</button>
+        <section id="results">
+        </section>
+
+        <!-- Modal-->
+        <div class="modals">
+            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalTitle">Information</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+
+                        <!-- Information will be added here-->
+                        <div class="modal-body">
+                            <div id="modalInfoContent1">
+                            </div>
+                            <!-- TODO: Delete modalInfoContent2 -->
+                            <div id="modalInfoContent2">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
