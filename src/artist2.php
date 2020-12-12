44ee8935783a67193b7055c28b92daf9eac422d2
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-12 21:16:14 +0100

    new search($searchText)

--- src/artist2.php
+++ src/artist2.php
@@ -51,4 +51,95 @@
             return $stmt->fetch();
         }
 
+        /**
+         * Retrieves the artists whose name includes a certain text
+         * 
+         * @param   searchText upon which to execute the search
+         * @return  an array with artists information
+         */
+        function search($searchText) {
+            $query = <<<'SQL'
+                SELECT ArtistId, Name
+                FROM artist
+                WHERE Name LIKE ?
+                ORDER BY Name;
+            SQL;
+
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute(['%' . $searchText . '%']);                
+
+            $this->disconnect();
+
+            return $stmt->fetchAll();                
+        }
+        
+        // /**
+        //  * Inserts a new Artist
+        //  * 
+        //  * @param   artist info
+        //  * @return  the ID of the new artist
+        //  */
+        // function create($info) {            
+        //     $query = <<<'SQL'
+        //         INSERT INTO artist (Name) VALUES (?);
+        //         SQL;
+
+        //     $stmt = $this->pdo->prepare($query);
+        //     $stmt->execute([$info['name']]);
+        //     $newID = $this->pdo->lastInsertId();
+            
+        //     $this->disconnect();
+        //     return $newID;
+        // }
+
+        // /**
+        //  * Updates an Artist
+        //  * 
+        //  * @param   artist info
+        //  * @return  true if success, -1 otherwise
+        //  */
+        // function update($info) {
+        //     try {
+        //         $query = <<<'SQL'
+        //         UPDATE artist
+        //             SET Name = ?
+        //             WHERE ArtistId = ?
+        //         SQL;
+
+        //         $stmt = $this->pdo->prepare($query);
+        //         $stmt->execute([$info['name'], $info['id']]);
+        //         $return = true;
+
+        //     } catch (Exception $e) {
+        //         $return = -1;
+        //         debug($e);
+        //     }
+        //     $this->disconnect();
+        //     return $return;
+        // }
+
+        // /**
+        //  * Deletes an Artist
+        //  * 
+        //  * @param   ID of the artist to delete
+        //  * @return  true if success, -1 otherwise
+        //  */
+        // function delete($id) {            
+        //     try {
+        //         $query = <<<'SQL'
+        //             DELETE 
+        //             FROM artist 
+        //             WHERE ArtistId = ?;
+        //         SQL;
+        //         $stmt = $this->pdo->prepare($query);
+        //         $stmt->execute([$id]);
+        //         $return = true;
+        //     } catch (Exception $e) {
+        //         $return = -1;
+        //         debug($e);
+        //     }
+        //     $this->disconnect();
+        //     return $return;
+        // }
+    }
 ?>
