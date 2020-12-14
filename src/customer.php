9fdd8bf98bc33caa36565e1b63390aa8c18e87e8
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-14 03:37:10 +0100

    Customer - create()

--- src/customer.php
+++ src/customer.php
@@ -122,5 +122,74 @@
             http_response_code(200);
             return $stmt->fetchAll();                
         }
+        
+        /**
+         * Insert a new Customer
+         * 
+         * @param   firstName, lastName, password, company, address, 
+         *          city, state, country, postalCode, phone, fax, email
+         * @return  the Id of the new Customer, 
+         *          -1 if the FirstName is null
+         *          -2 if the LastName is null
+         *          -3 if the Password is null
+         *          -4 if the Email is null
+         *          -5 if the Customer email already exists
+         *          -6 if the Customer could not be created
+         */
+        function create($firstName, $lastName, $password, $company, 
+                        $address, $city, $state, $country, $postalCode, $phone, $fax, $email) {
+            
+            //  Check if FirstName, LastName, Password, Email are null
+            if ($firstName == null) {
+                http_response_code(409);
+                return -1;
+            } else if ($lastName == null) {
+                http_response_code(409);
+                return -2;
+            } else if ($Password == null) {
+                http_response_code(409);
+                return -3;
+            } else if ($Email == null) {
+                http_response_code(409);
+                return -4;
+            }
+
+            // Check the count of Customers with this Email
+            $query = <<<'SQL'
+                SELECT COUNT(*) AS total FROM Customer WHERE Email = ?;
+            SQL;
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute([$email]);   
+
+            if ($stmt->fetch()['total'] > 0) {
+                // Customer Email already exists
+                http_response_code(409);
+                return -5;
+            }
+            
+            // Create Customer
+            try {
+                $query = <<<'SQL'
+                    INSERT INTO track (FirstName, LastName, Password, Company, Address, 
+                                    City, State, Country, PostalCode, Phone, Fax, Email) 
+                                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+                    SQL;
+
+                $stmt = $this->pdo->prepare($query);
+                $stmt->execute([$firstName, $lastName, $password, $company, $address, 
+                                $city, $state, $country, $postalCode, $phone, $fax, $email]);
+                $newID = $this->pdo->lastInsertId();
+                $return = $newID;
+
+            } catch (Exception $e) {
+                http_response_code(500);
+                $return = -6;
+                debug($e);
+            }
+            
+            $this->disconnect();
+            http_response_code(200);
+            return $return;
+        }
     }
 ?>
