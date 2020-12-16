41477d38d81a1b14ace0829d16d3d1196fe89347
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-16 07:58:42 +0100

    Library Artists Page

--- user/library-artists.php
+++ user/library-artists.php
@@ -21,8 +21,73 @@
     $email = $_SESSION['email'];
 ?>
 
+
 <!-- Start of Page content -->
-<div class="container">
+<div class="container" id="page-artists">
+
+    <!-- Sidebar navigation -->
+    <div class="sidebar">
+        <button type="button" class="btn btn-primary scrollUp">Up</button>
+        <button type="button" class="btn btn-primary scrollDown">Down</button>
+    </div>
+
+
+    <h1 class="main-title">Library Artists</h1>
+    <div class="resultArea">
+        <!-- <button type="button" class="btn btn-success mb-2 createArtistModal" 
+                id="btnAdd" data-toggle='modal' data-target='#modal'>Create Artist</button> -->
+        <!-- <button type="button" class="btn btn-success mb-2" id="btnShowArtists">Show All Artists</button> -->
+        <label for="txtName" id="txtNameLabel">Artist Name</label>
+        <input type="text" id="searchArtistName" name="Name" required>
+        <button type="button" class="btn btn-success mb-2" id="btnSearchArtist">Search Artist</button>
+
+        <div class="buttons">
+            <button type="button" class="btn btn-primary mb-2 btnLibrary  active" 
+                onclick="window.location.href='/WAD-MA2/user/library-artists.php'">Artists
+            </button>
+            <button type="button" class="btn btn-primary mb-2 btnLibrary" 
+                onclick="window.location.href='/WAD-MA2/user/library-albums.php'">Albums
+            </button>
+            <button type="button" class="btn btn-primary mb-2 btnLibrary" 
+                onclick="window.location.href='/WAD-MA2/user/library-tracks.php'">Tracks
+            </button>
+        </div>
+        
+        <section id="artistResults">
+        </section>
+
+        <!-- Modal-->
+        <div class="modals">
+            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalTitle">Information</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+
+                        <!-- Information will be added here-->
+                        <div class="modal-body">
+                            <div id="modalInfoContent1">
+                            </div>
+                            <!-- TODO: Delete modalInfoContent2 -->
+                            <div id="modalInfoContent2">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Start of Page content -->
+<!-- <div class="container">
     <h1 class="main-title">Library Artists</h1>
     <div class="resultArea">
         <div class="buttons">
@@ -71,7 +136,7 @@
             </tbody>
         </table>
     </div>
-</div>
+</div> -->
 
 <!-- End of Page content -->
 
