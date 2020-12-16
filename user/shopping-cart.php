766cc251da189c521f0df76d74a715118ce36ff2
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-16 07:16:10 +0100

    Shopping Cart page

--- user/shopping-cart.php
+++ user/shopping-cart.php
@@ -22,8 +22,82 @@
 
 ?>
 
+
 <!-- Start of Page content -->
-<div class="container">
+<div class="container" id="page-tracks">
+
+    <?php
+        print_r($_SESSION['ShoppingCart']);
+    ?>
+    <script type="text/javascript">
+        var shoppingCart = <?php echo json_encode($_SESSION['ShoppingCart']); ?>;
+        console.log(shoppingCart);
+    </script>
+
+    <!-- Sidebar navigation -->
+    <div class="sidebar">
+        <button type="button" class="btn btn-primary scrollUp">Up</button>
+        <button type="button" class="btn btn-primary scrollDown">Down</button>
+    </div>
+
+    <h1 class="main-title">Shopping Cart</h1>
+    <div class="resultArea">
+
+        <!-- <div class="buttons">
+            <button type="button" class="btn btn-primary mb-2 btnLibrary" 
+                onclick="window.location.href='/WAD-MA2/user/library-artists.php'">Artists
+            </button>
+            <button type="button" class="btn btn-primary mb-2 btnLibrary" 
+                onclick="window.location.href='/WAD-MA2/user/library-albums.php'">Albums
+            </button>
+            <button type="button" class="btn btn-primary mb-2 btnLibrary active" 
+                onclick="window.location.href='/WAD-MA2/user/library-tracks.php'">Tracks
+            </button>
+        </div> -->
+
+        <!-- <button type="button" class="btn btn-success mb-2 createTrackModal"  -->
+                <!-- id="btnAdd" data-toggle='modal' data-target='#modal'>Create Tracks</button> -->
+        <!-- <button type="button" class="btn btn-success mb-2" id="btnShowArtists">Show All Artists</button> -->
+        <!-- <label for="txtName" id="txtNameLabel">Track Name</label>
+        <input type="text" id="searchTrackName" name="Name" required>
+        </br>
+        <button type="button" class="btn btn-success mb-2" id="btnSearchTrack">Search Track</button> -->
+        <section id="results">
+        </section>
+
+        <!-- Modal-->
+        <div class="modals">
+            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalTitle">Information</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+
+                        <!-- Information will be added here-->
+                        <div class="modal-body">
+                            <div id="modalInfoContent1">
+                            </div>
+                            <!-- TODO: Delete modalInfoContent2 -->
+                            <div id="modalInfoContent2">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Start of Page content -->
+<!-- <div class="container">
     <h1 class="main-title">Shopping Cart</h1>
     <div class="resultArea">
         <table class="table tableList">
@@ -80,7 +154,7 @@
             </tbody>
         </table>
     </div>
-</div>
+</div> -->
 
 <!-- End of Page content -->
 
