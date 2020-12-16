cdb24515fe860967c6bad6dadae19f1f08000ad0
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-16 03:43:57 +0100

    sign up - login session

--- user/sign-up.php
+++ user/sign-up.php
@@ -5,20 +5,26 @@
 <?php
     require_once('../src/functions.php');
 
-    session_start();
-    debug($_SESSION);
+    // session_start();
+    // debug($_SESSION);
 
-    if (!isset($_SESSION['userID'])) {
-        header('Location: ../auth/login.php');
-    }
-    $userID = $_SESSION['userID'];
-    $firstName = $_SESSION['firstName'];
-    $lastName = $_SESSION['lastName'];
-    $email = $_SESSION['email'];
+    // if (!isset($_SESSION['userID'])) {
+    //     header('Location: ../auth/login.php');
+    // }
+    // $userID = $_SESSION['userID'];
+    // $firstName = $_SESSION['firstName'];
+    // $lastName = $_SESSION['lastName'];
+    // $email = $_SESSION['email'];
+    // echo $userID . ' -  ';
+    // echo $firstName . ' -  ';
+    // echo $lastName . ' -  ';
+    // echo $email . ' -  ';
 ?>
 
 <!-- Start of Page content -->
 <div class="container">
+
+
     <h1 class="main-title">Sign Up</h1>
     <div class="resultArea">
         
