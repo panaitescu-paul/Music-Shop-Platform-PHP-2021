38d1eac53ec054d3ea50685c70f9a035b9562000
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-17 16:19:26 +0100

    Content-Security-Policy

--- fragments/header-login.php
+++ fragments/header-login.php
@@ -4,12 +4,13 @@
     <title>Music Shop</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta http-equiv="Content-Security-Policy" content="script-src 'self'; object-src 'self';"> -->
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="../css/bootstrap.css">
     <script src="../js/jquery-3.5.1.js"></script>
     <script src="../js/script.js"></script>
     <script src="../js/bootstrap.js"></script>
-    <script src="../js/dom.js"></script>
+    <!-- <script src="../js/dom.js"></script> -->
     <script src="../js/functions.js"></script>
 </head>
 <body>
