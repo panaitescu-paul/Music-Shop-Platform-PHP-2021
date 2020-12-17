bc3a8f6c3b8fcb13ee8bbc669eb2ab2aa31299b5
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-12-17 06:36:01 +0100

    DB - AWS version and Local version

--- src/conn_data.php
+++ src/conn_data.php
@@ -1,13 +1,13 @@
 <?php
     // Local db
-    $server = 'localhost';
-    $dbName = 'chinook_abridged';
-    $user = 'root';
-    $pwd = '';
+    // $server = 'localhost';
+    // $dbName = 'chinook_abridged';
+    // $user = 'root';
+    // $pwd = '';
 
     // AWS db
-    // $server = 'dbfilms.cqnxkgorpvhz.us-east-1.rds.amazonaws.com';
-    // $dbName = 'films';
-    // $user = 'adminfilms';
-    // $pwd = 'technology';
+    $server = 'music-shop-db.c8wp1nlxpzmc.us-east-1.rds.amazonaws.com';
+    $dbName = 'chinook_abridged';
+    $user = 'paul5173';
+    $pwd = 'password1';
 ?>
