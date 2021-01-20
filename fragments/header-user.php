be080e29517e1da8b3e82cdeb7ace744534b99a7
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2021-01-20 04:44:39 +0100

    Changed name to "Music Shop"

--- fragments/header-user.php
+++ fragments/header-user.php
@@ -18,7 +18,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Music Marketplace</title>
+    <title>Music Shop</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
  
